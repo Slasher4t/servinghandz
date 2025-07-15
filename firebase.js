6cca6e9e58c25aaaa2ba64797cf970c46b434da8
Slasher4t <38390516+Slasher4t@users.noreply.github.com>
2025-07-15 15:41:40 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,14 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyA7_MQUYibk47xVL-Y1Msg5WJ2KDcPlrCw',
-  authDomain: 'servinghandz.firebaseapp.com',
-  projectId: 'servinghandz',
-  storageBucket: 'servinghandz.firebasestorage.app',
-  messagingSenderId: '192466884693',
-  appId: '1:192466884693:web:0fa333196e4760aae7dcdc',
-  measurementId: 'G-SMF25HCLJY'
 };
 
 const app = initializeApp(firebaseConfig);
